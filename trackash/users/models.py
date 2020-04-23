9acc1f59091420c1ce454a29271b2aadd6797ca3
sebas_black <joseb.twelve@gmail.com>
2020-04-23 07:55:44 -0500

    #task009 - added configuration for delete old image_profile

--- trackash/users/models.py
+++ trackash/users/models.py
@@ -2,11 +2,20 @@ from django.contrib.auth.models import AbstractUser
 from django.db.models import CharField, ImageField
 from django.urls import reverse
 from django.utils.translation import ugettext_lazy as _
+from django.core.files.storage import FileSystemStorage
+from django.conf import settings
+import os
 
 
 def user_directory_path(instance, filename):
     # file will be uploaded to MEDIA_ROOT / user_<id>/<filename>
-    return f"user_{instance.id}/{filename}"
+    extension = filename.split('.')[1]
+    fullname = os.path.join(settings.MEDIA_ROOT, f"default/default_image.{extension}")
+
+    if os.path.exists(fullname):
+        os.remove(fullname)
+    
+    return f"default/default_image.{extension}"
 
 
 class User(AbstractUser):
@@ -15,7 +24,10 @@ class User(AbstractUser):
     # around the globe.
     name = CharField(_("Name of User"), blank=True, max_length=255)
     image_profile = ImageField(
-        _("Image Profile"), blank=True, null=True, upload_to=user_directory_path
+        _("Image Profile"),
+        blank=True,
+        null=True,
+        upload_to=user_directory_path,
     )
 
     def get_absolute_url(self):
