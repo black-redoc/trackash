54e5fec133a3aad8d68036cd5247a9c727a96907
sebas_black <joseb.twelve@gmail.com>
2020-04-23 05:38:42 -0500

    #task009 - added DRF-EXTRA-FIELDS to user serializers.py

--- trackash/users/serializers.py
+++ trackash/users/serializers.py
@@ -1,10 +1,17 @@
+from django.conf import settings
 from django.contrib.auth import get_user_model
+from drf_extra_fields.fields import Base64ImageField
 from rest_framework import serializers
 
 User = get_user_model()
 
 
-class ProfileSerializer(serializers.HyperlinkedModelSerializer):
+class ProfileSerializer(serializers.ModelSerializer):
+
+    username = serializers.CharField(required=False)
+    email = serializers.EmailField(required=False)
+    image_profile = Base64ImageField(required=False)
+
     class Meta:
         model = User
         fields = (
